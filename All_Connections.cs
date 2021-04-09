12116e0ae4192bb6e1509c2771a65ebca36afc89
Ionsai <saikmr106@gmail.com>
2021-04-09 17:01:28 +0530

    Add files via upload

---
+++ All_Connections.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace MVC_Pratice
+{
+    public class All_Connections
+    {
+
+        SqlConnection con = null;
+         public   SqlConnection init_connection_1 ()
+        {
+
+            String conString = "server=192.168.23.226;user id=sa;password=sandbox@gitam@cats;database=Gitam";
+
+            try
+            {
+                con = new SqlConnection(conString);
+            }
+            catch(SqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            return con;
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Pratice.MvcApplication" Language="C#" %>
